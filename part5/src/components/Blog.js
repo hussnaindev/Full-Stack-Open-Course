bc5cdeadc25c57fe50c9547fb08951c84ea72d8b
Hussnain Raza ⚡ <hussnain444all@gmail.com>
2021-11-21 15:01:24 +0500

    Blog view and hide visibility added

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 const Blog = ({blog}) => {
+
+  const [blogInfoVisible,setBlogInfoVisible] = useState(false)
+
+  const hideWhenVisible = {display: blogInfoVisible ? 'none':''}
+  const showWhenVisible = {display: blogInfoVisible ? '':'none'}
+
+  const buttonText = {value: blogInfoVisible ? 'hide':'view'}
+
+  const toggleVisibilty = () => setBlogInfoVisible(!blogInfoVisible)
   console.log(blog)
   return(
     
-      <div>
-        {blog.title} {blog.author}
+      <div className="blogList">
+        <div >
+            <span >
+              {blog.title} {blog.author} 
+            </span>
+            <span>
+              <button onClick={toggleVisibilty}>{buttonText.value}</button>
+            </span>
+        </div>
+
+        <div style={showWhenVisible}>
+            <p>{blog.url}</p>
+            <span>{blog.likes} <button>likes</button></span>
+            <p>{blog.user.name}</p>
+           
+        </div>
+        
       </div>
   )
 }
