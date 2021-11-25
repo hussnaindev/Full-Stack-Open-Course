830523ccbfda754b65b7269583390bf0c1d471a2
Hussnain Raza ⚡ <hussnain444all@gmail.com>
2021-11-25 23:44:52 +0500

    react-redux state management

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import CounterReducer from './CounterReducer'
+
+console.log('index file rendering')
+
+const store = createStore(CounterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
